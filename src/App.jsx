fcbd63fd26ec7c9f6ee507860518832e455237de
Kevin Li <kevin.li128@gmail.com>
2023-11-01 10:16:31 -0700

    added context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router';
 import Navbar from './components/Navbar';
 
 const App = () => {
+    const [language, setLanguage] = useState(true)
+
   return (
     <div className="px-6 min-w-[448px] max-w-5xl m-auto">
-      <Navbar />
-      <Outlet />
+      <Navbar language={language} setLanguage={setLanguage}/>
+      <Outlet context={[language, setLanguage]}/>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { language, setLanguage } = props;
   const linkStyle = 'pt-4 pr-4 inline-block hover:underline underline-offset-4';
   return (
     <div className="flex place-content-between m-auto max-w-5xl">
       <div>
-      <Link to={'/search'} className={linkStyle}>
+        <Link to={'/search'} className={linkStyle}>
           Search
         </Link>
         <Link to={'/'} className={linkStyle}>
@@ -19,10 +20,14 @@ const Navbar = () => {
           Transactions
         </Link> */}
       </div>
-
-      <Link to={'/vehicle/new'} className={linkStyle}>
-        New
-      </Link>
+      <div>
+        <button className="pt-4 pr-4 inline-block hover:underline underline-offset-4" onClick={() => setLanguage(!language)}>
+          {language ? 'English' : '中文'}
+        </button>
+        <Link to={'/vehicle/new'} className={linkStyle}>
+          New
+        </Link>
+      </div>
     </div>
   );
 };

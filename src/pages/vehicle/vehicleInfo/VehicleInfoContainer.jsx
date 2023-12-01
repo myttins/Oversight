bc91ba8b4dadc466f47eea6709a8b548c6a4ef57
Kevin Li <kevin.li128@gmail.com>
2023-12-01 12:31:29 -0800

    fixed vehicle edit

--- src/pages/vehicle/vehicleInfo/VehicleInfoContainer.jsx
+++ src/pages/vehicle/vehicleInfo/VehicleInfoContainer.jsx
@@ -16,37 +16,45 @@ const VehicleInfoContainer = ({ vehicleInfo, setVehicleInfo }) => {
   const [readOnly, setReadOnly] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
 
+  const [vehicle, setVehicle] = useState(vehicleInfo);
+
   const { showBanner } = useContext(MessageBannerContext);
 
   const handleVehicleInfoSave = async () => {
     try {
-      await axios.post(`/api/vehicle/${vehicleInfo.id}`, vehicleInfo);
+      await axios.post(`/api/vehicle/${vehicle.id}`, vehicle);
+      setReadOnly(true);
+      showBanner({ style: 'success', message: 'Vehicle edited successfully' });
     } catch (error) {
-      console.error('Error making POST request:', error);
+      console.error(error);
       showBanner({ style: 'error' });
     }
+  };
+
+  const handleCancelVehicleEdit = () => {
     setReadOnly(true);
+    setVehicle(vehicleInfo);
   };
 
-  const inputIsValid = () => {
-    if (
-      !vehicleInfo.plate ||
-      !vehicleInfo.vehicle_model ||
-      !vehicleInfo.category ||
-      !vehicleInfo.vehicle_model ||
-      !vehicleInfo.engine_no ||
-      !vehicleInfo.vehicle_color ||
-      !vehicleInfo.vin ||
-      !vehicleInfo.notes ||
-      !vehicleInfo.operating_license_no ||
-      !vehicleInfo.fuel_type
-    ) {
-      setErrorMessage('Invalid Input');
-      return false;
-    }
+  // const inputIsValid = () => {
+  //   if (
+  //     !vehicleInfo.plate ||
+  //     !vehicleInfo.vehicle_model ||
+  //     !vehicleInfo.category ||
+  //     !vehicleInfo.vehicle_model ||
+  //     !vehicleInfo.engine_no ||
+  //     !vehicleInfo.vehicle_color ||
+  //     !vehicleInfo.vin ||
+  //     !vehicleInfo.notes ||
+  //     !vehicleInfo.operating_license_no ||
+  //     !vehicleInfo.fuel_type
+  //   ) {
+  //     setErrorMessage('Invalid Input');
+  //     return false;
+  //   }
 
-    return true;
-  };
+  //   return true;
+  // };
 
   return (
     <div className='p-4 bg-white my-4'>
@@ -62,7 +70,7 @@ const VehicleInfoContainer = ({ vehicleInfo, setVehicleInfo }) => {
           />
         ) : (
           <div>
-            <button className='btn mx-2' onClick={() => setReadOnly(!readOnly)}>
+            <button className='btn mx-2' onClick={handleCancelVehicleEdit}>
               CANCEL
             </button>
 
@@ -73,47 +81,35 @@ const VehicleInfoContainer = ({ vehicleInfo, setVehicleInfo }) => {
         )}
       </div>
       <div className='px-4'>
-        <FormElement
-          label={'category'}
-          type={'text'}
-          readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
-        />
+        <FormElement label={'category'} type={'text'} readOnly={readOnly} formInfo={vehicle} setFormInfo={setVehicle} />
         <FormElement
           label={'vehicle_model'}
           type={'text'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
         <FormElement
           label={'engine_no'}
           type={'text'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
         <FormElement
           label={'vehicle_color'}
           type={'text'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
-        <FormElement
-          label={'vin'}
-          type={'text'}
-          readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
-        />
+        <FormElement label={'vin'} type={'text'} readOnly={readOnly} formInfo={vehicle} setFormInfo={setVehicle} />
         <FormElement
           label={'operating_license_no'}
           type={'text'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
         <FormElement
           label={'fuel_type'}
@@ -123,29 +119,29 @@ const VehicleInfoContainer = ({ vehicleInfo, setVehicleInfo }) => {
             { label: 'ELECTRIC', value: 'ELECTRIC' },
           ]}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
         <FormElement
           label={'registration_date'}
           type={'date'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
         {/* <FormElement
           label={'activation_date'}
           type={'date'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         /> */}
         <FormElement
           label={'notes'}
           type={'textarea'}
           readOnly={readOnly}
-          formInfo={vehicleInfo}
-          setFormInfo={setVehicleInfo}
+          formInfo={vehicle}
+          setFormInfo={setVehicle}
         />
       </div>
       {errorMessage.length > 0 ? <ErrorMessage message={errorMessage} /> : null}
